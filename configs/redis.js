d2fa6db216fb2f581b5b1e3e119d1f46a806fa20
John Garland <johnpg82@gmail.com>
2022-12-27 23:47:07 -0700

    For Heroku Use
    For Heroku Use

--- configs/redis.js
+++ configs/redis.js
@@ -1,6 +1,9 @@
 const { createClient } = require('redis');
 
 // Socket required for node redis <-> docker-compose connection
-const Redis = createClient({ socket: { host: process.env.REDIS_URL, port: 6379 } });
+
+//const Redis = createClient({ socket: { host: 'redis', port: 6379 } });
+
+const Redis = createClient({ socket: { url: process.env.REDIS_URL} });
 
 module.exports = Redis;
